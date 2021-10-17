24877921cb5e45a4f4305a8710124023a8a80c5f
Kengo Higashi <shigure@higashi1999.onmicrosoft.com>
2021-10-18 01:25:05 +0900

    init package

--- ui/src/pages/_app.tsx
+++ ui/src/pages/_app.tsx
@@ -1,7 +1,55 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as React from 'react';
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import createCache from '@emotion/cache';
+import { createTheme } from '@mui/material/styles';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const theme = createTheme({
+    typography: {
+        fontFamily: [
+            '"Lato"',
+            '"Noto Sans JP"',
+            '"Hiragino Kaku Gothic ProN"',
+            'Meiryo',
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(',')
+    },
+});
+
+function createEmotionCache() {
+    return createCache({ key: 'css' });
 }
-export default MyApp
+
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+    emotionCache?: EmotionCache;
+}
+
+export default function MyApp(props: MyAppProps) {
+    const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+    return (
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <title>Aion</title>
+                <meta name="viewport" content="initial-scale=1, width=device-width" />
+            </Head>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </CacheProvider>
+    );
+}
---
+++ ui/src/pages/_document.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import createEmotionServer from '@emotion/server/create-instance';
+import createCache from '@emotion/cache';
+
+function createEmotionCache() {
+    return createCache({ key: 'css' });
+}
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="ja">
+                <Head>
+                <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+    const originalRenderPage = ctx.renderPage;
+    const cache = createEmotionCache();
+    const { extractCriticalToChunks } = createEmotionServer(cache);
+
+    ctx.renderPage = () =>
+        originalRenderPage({
+            enhanceApp: (App: any) => (props) => <App emotionCache={cache} {...props} />,
+        });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    const emotionStyles = extractCriticalToChunks(initialProps.html);
+    const emotionStyleTags = emotionStyles.styles.map((style) => (
+        <style
+            data-emotion={`${style.key} ${style.ids.join(' ')}`}
+            key={style.key}
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{ __html: style.css }}
+        />
+    ));
+
+    return {
+        ...initialProps,
+        styles: [...React.Children.toArray(initialProps.styles), ...emotionStyleTags],
+    };
+};
